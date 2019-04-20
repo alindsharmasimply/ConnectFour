909b43ae5299d3762385f0d02bae61b8d20193c7
alindsharmasimply <alindsharmasimply@gmail.com>
2019-04-20 06:29:15 +0530

    Successfully created the blueprint of the matrix

--- connectFour.py
+++ connectFour.py
@@ -1,22 +1,51 @@
 import numpy as np
 
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
 def createBoard():
     board = np.zeros((6, 7))
     return board
 
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col):
+    return board[5][col] == 0
+    
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def print_board(board):
+    print(np.flip(board, 0))
+
 board = createBoard()
+print_board(board)
 game_over = False
 turn = 0
 
 while not game_over:
     
-    #Ask for Player 1 Input
+    #Ask for Player 1's Input
     if turn == 0:
-        selection =  int(input("Player 1 make your selction (0 - 6)"))
+        col =  int(input("Player 1 make your selction (0 - 6)"))
+        # 'col' represents the column where the player will drop the coin
         
-    #Ask for Player 2 Input
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+        
+    #Ask for Player 2's Input
     else:
-        selection =  int(input("Player 2 make your selction (0 - 6)"))
+        col =  int(input("Player 2 make your selction (0 - 6)"))
+        # 'col' represents the column where the player will drop the coin
         
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+    
+    print_board(board)
     turn += 1
     turn = turn % 2
